3066d4041bd00b476f85452467fdb2285562f569
Vince Liao <vinvancan@gmail.com>
2020-02-20 17:34:31 -0800

    add themes

---
+++ appfront/theme/terry/theme01/catalog/product/index/payment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+?>
+<div class="scroll_left">
+    <a href=""><?= Yii::$service->page->translate->__('支付方式:'); ?></a>
+</div>
+<p><?= Yii::$service->page->translate->__('HUATANTEST.com接受支付宝、微信支付和银行卡付款。'); ?></p>
+
+<p><img alt="" height="407" src="<?= Yii::$service->image->getImgUrl('appfront/images/accept_payment_methods.png') ?>" width="600"></p>
