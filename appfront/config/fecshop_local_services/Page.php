622ab86f8dcae52f8598c77ba6727df288b2bc62
Vince Liao <vinvancan@gmail.com>
2020-04-07 20:13:22 -0700

    remove custom menu

--- appfront/config/fecshop_local_services/Page.php
+++ appfront/config/fecshop_local_services/Page.php
@@ -95,32 +95,32 @@ return [
                 /*
                  *	在菜单后面部分（产品分类菜单的前面部分）的自定义菜单
                  */
-                'behindCustomMenu' => [
-                    [
-                        'name'        => 'custom menu',            // 菜单名字
-                        'urlPath'     => '/my-custom-menu.html',    // 菜单对应的url
-                        'childMenu'   => [                        // 子菜单
-                            [
-                                'name'        => 'my custom menu 2',
-                                'urlPath'     => '/my-custom-menu-2.html',
-                            ],
-                            [
-                                'name'        => 'my custom menu 3',
-                                'urlPath'     => '/my-custom-menu-2.html',
-                                'childMenu'   => [
-                                    [
-                                        'name'        => 'my custom menu 3',
-                                        'urlPath'     => '/my-custom-menu-2.html',
-                                    ],
-                                    [
-                                        'name'        => 'my custom menu 3',
-                                        'urlPath'     => '/my-custom-menu-2.html',
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
+//                'behindCustomMenu' => [
+//                    [
+//                        'name'        => 'custom menu',            // 菜单名字
+//                        'urlPath'     => '/my-custom-menu.html',    // 菜单对应的url
+//                        'childMenu'   => [                        // 子菜单
+//                            [
+//                                'name'        => 'my custom menu 2',
+//                                'urlPath'     => '/my-custom-menu-2.html',
+//                            ],
+//                            [
+//                                'name'        => 'my custom menu 3',
+//                                'urlPath'     => '/my-custom-menu-2.html',
+//                                'childMenu'   => [
+//                                    [
+//                                        'name'        => 'my custom menu 3',
+//                                        'urlPath'     => '/my-custom-menu-2.html',
+//                                    ],
+//                                    [
+//                                        'name'        => 'my custom menu 3',
+//                                        'urlPath'     => '/my-custom-menu-2.html',
+//                                    ],
+//                                ],
+//                            ],
+//                        ],
+//                    ],
+//                ],
             ],
         ],
     ],
